9c56143b50287628dc0859ff5265b7801b004595
Greg Murray <gd-murray@hotmail.com>
2024-04-10 07:19:41 -0700

    add mongo-express credentials, remove getSibling

--- scraper/init-mongo.js
+++ scraper/init-mongo.js
@@ -1,5 +1,5 @@
 console.log("INITIALIZING MONGO USER AND DATABASE: ", process.env["MONGODB_USER"], process.env["MONGODB_NAME"])
-db = db.getSiblingDB(process.env["MONGODB_NAME"])
+// db = db.getSiblingDB(process.env["MONGODB_NAME"])
 db.createUser({
     user: process.env["MONGODB_USER"],
     password: process.env["MONGODB_PASSWORD"],
