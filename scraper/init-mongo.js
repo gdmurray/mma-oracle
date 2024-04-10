b6aa4ee678c8a6a7b621ac025b30ed7503a54898
Greg Murray <gd-murray@hotmail.com>
2024-04-10 10:11:37 -0700

    Redo deployment, again

--- scraper/init-mongo.js
+++ scraper/init-mongo.js
@@ -1,7 +1,17 @@
 console.log("INITIALIZING MONGO USER AND DATABASE: ", process.env["MONGODB_USER"], process.env["MONGODB_NAME"])
-// db = db.getSiblingDB(process.env["MONGODB_NAME"])
-db.createUser({
+var rootUser = process.env["MONGO_INITDB_ROOT_USERNAME"]
+var rootPass = process.env["MONGO_INITDB_ROOT_PASSWORD"]
+var admin = db.getSiblingDB('admin');
+admin.auth(rootUser, rootPass);
+console.log("AUTHENTICATED WITH ROOT USER")
+var user = process.env["MONGODB_USER"]
+var pass = process.env["MONGODB_PASSWORD"]
+db = db.getSiblingDB("mmaoracledb")
+console.log("GOT NEW DATABASE");
+const result = db.createUser({
     user: process.env["MONGODB_USER"],
     password: process.env["MONGODB_PASSWORD"],
-    roles: [{role: 'readWrite', db: process.env["MONGODB_NAME"]}],
+    roles: [{roles: ['readWrite'], db: process.env["MONGODB_NAME"]}],
 })
+
+console.log("RESULT: ", result);
---
+++ scraper/init-mongo.sh
@@ -0,0 +1,8 @@
+set -e
+
+mongo <<EOF
+var rootUser = '$MONGO_INITDB_ROOT_USERNAME';
+var rootPassword =
+
+EOF
+
