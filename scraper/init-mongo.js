a4e5e1a3f2f4572747776d7a9a65707eab827c83
Greg Murray <gd-murray@hotmail.com>
2024-04-10 10:21:00 -0700

    maybe this time it'll work

--- scraper/init-mongo.js
+++ scraper/init-mongo.js
@@ -1,17 +1,17 @@
 console.log("INITIALIZING MONGO USER AND DATABASE: ", process.env["MONGODB_USER"], process.env["MONGODB_NAME"])
-var rootUser = process.env["MONGO_INITDB_ROOT_USERNAME"]
-var rootPass = process.env["MONGO_INITDB_ROOT_PASSWORD"]
-var admin = db.getSiblingDB('admin');
-admin.auth(rootUser, rootPass);
-console.log("AUTHENTICATED WITH ROOT USER")
-var user = process.env["MONGODB_USER"]
-var pass = process.env["MONGODB_PASSWORD"]
+// var rootUser = process.env["MONGO_INITDB_ROOT_USERNAME"]
+// var rootPass = process.env["MONGO_INITDB_ROOT_PASSWORD"]
+// var admin = db.getSiblingDB('admin');
+// admin.auth(rootUser, rootPass);
+// console.log("AUTHENTICATED WITH ROOT USER")
+// var user = process.env["MONGODB_USER"]
+// var pass = process.env["MONGODB_PASSWORD"]
 db = db.getSiblingDB("mmaoracledb")
-console.log("GOT NEW DATABASE");
+// console.log("GOT NEW DATABASE");
 const result = db.createUser({
     user: process.env["MONGODB_USER"],
-    password: process.env["MONGODB_PASSWORD"],
+    pwd: process.env["MONGODB_PASSWORD"],
     roles: [{roles: ['readWrite'], db: process.env["MONGODB_NAME"]}],
 })
 
-console.log("RESULT: ", result);
+// console.log("RESULT: ", result);
